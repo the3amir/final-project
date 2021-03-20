1e72bc91631b26bf343ac3e931a7ae853b9c867f
Aamir Abbas Altaiy <aimer.abbas33@gmail.com>
2021-03-21 02:19:18 +0300

    new edit

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,6 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_mail import Mail
-from flask_mail import smtplib
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5c32252213a16297ab251437f263f8c0'
@@ -12,4 +8,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 from application import routes
-
